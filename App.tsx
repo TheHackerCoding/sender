52f57e7690772f2317e3e7646f8319a4a84f184c
TheHackerCoding <triniziah@gmail.com>
2021-10-11 22:41:53 -0400

    uhhh

--- App.tsx
+++ App.tsx
@@ -1,31 +1,34 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { Button, StyleSheet, Text, View } from 'react-native';
-import constants from './constants';
-import startup from './startup';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import constants from "./constants";
+import startup from "./startup";
 
 export default function App() {
-  startup()
-  return (
-    <Uhh />
-  );
+  startup();
+  return <Uhh />;
 }
 
 const Uhh = () => {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <Button></Button>
+      <View
+        style={{
+          flexDirection: "row",
+          flexWrap: "wrap",
+        }}
+      ></View>
       <StatusBar style="auto" />
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: constants.mainColor,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
